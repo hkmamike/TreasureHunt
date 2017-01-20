b00e8f40f21e9ef63cef601c584b5bf042d9f782
AK <AKwoKWH+Github@gmail.com>
2017-01-20 17:20:28 +0800

    update laylout of article list in test page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -216,6 +216,19 @@ angular.module('starter.controllers', [])
         $ionicScrollDelegate.scrollTop();
     };
 
+  // --------------------- Pie Chart Configuration -----------------------------
+  $scope.pieLabels = ["FB", "Twitter", "Instagram"];
+  $scope.pieData = [300, 500, 100];
+
+  // --------------------- Line Chart Configuration ----------------------------
+  $scope.lineSeries = ['Active', 'Inactive'];
+  $scope.lineLabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
+  $scope.lineData = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+
+
 })
 
 
