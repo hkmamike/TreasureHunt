29ab94641554983ba04a1008bdc698cfa5ab46c9
Brian Leung (Mike) <mikebrianleung@gmail.com>
2017-02-12 01:30:30 +0800

    Added img upload template and building directive for files upload for ng-model

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -145,7 +145,8 @@ angular.module('starter.controllers', [])
 
   $scope.pushArticle = function (article) {
     console.log('new article posted', article);
-    articles.saveArticle(article);
+
+    articles.saveImage(article);
 
     //clearing ng-model values after submit
     article.name = "";
@@ -326,6 +327,47 @@ angular.module('starter.controllers', [])
 
 })
 
+//Experimental directive built for file upload with ng-model
+.directive('appFilereader', function($q) {
+  var slice = Array.prototype.slice;
+
+  return {
+    restrict: 'A',
+    require: '?ngModel',
+    link: function(scope, element, attrs, ngModel) {
+      if (!ngModel) return;
+
+      ngModel.$render = function() {};
+
+      element.bind('change', function(e) {
+        var element = e.target;
+
+        $q.all(slice.call(element.files, 0).map(readFile))
+            .then(function(values) {
+                if (element.multiple) ngModel.$setViewValue(values);
+                else ngModel.$setViewValue(values.length ? values[0] : null);
+            });
+
+        function readFile(file) {
+            var deferred = $q.defer();
+
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                deferred.resolve(e.target.result);
+            };
+            reader.onerror = function(e) {
+                deferred.reject(e);
+            };
+            reader.readAsDataURL(file);
+
+            return deferred.promise;
+        }
+
+      }); //change
+
+    }
+  };
+})
 
 //Shrink2Level------------------------------------------------------------------------------------
 .directive('headerShrink2', function($document) {
--- www/js/services.js
+++ www/js/services.js
@@ -248,7 +248,7 @@ angular.module('starter.services', [])
 
     },
 
-    saveArticle: function(article) {
+    saveArticle: function(downloadURL, article) {
       console.log('will save this to the database', article);
       var uid = userData.getUser().uid;
       console.log(uid);
@@ -267,7 +267,7 @@ angular.module('starter.services', [])
         contents: article.contents,
         articleImgs: newArticleImg,
         timestamp: Math.floor(Date.now()/1000),
-        coverImage: 'https://firebasestorage.googleapis.com/v0/b/sponsar-4497d.appspot.com/o/squareFood3.jpg?alt=media&token=1dd9a04c-3452-4732-91b2-68abdfa1cf2b',
+        coverImage: downloadURL,
         author: uid,
         upVote: 1,
         downVote: 0,
@@ -286,6 +286,57 @@ angular.module('starter.services', [])
       updates['/users/' + uid + '/posts/' + newPostKey] = newPostKey;
 
       return firebase.database().ref().update(updates);
+    },
+
+    saveImage: function(article) {
+      // File or Blob named mountains.jpg
+      var file = article.image;
+
+      console.log('file is : ', article);
+
+      // Create the file metadata
+      var metadata = {
+        contentType: 'image/jpeg'
+      };
+
+      var storageRef = firebase.storage().ref();
+
+      // Upload file and metadata to the object 'images/mountains.jpg'
+      var uploadTask = storageRef.child('images/' + file.name).put(file, metadata);
+
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+        function(snapshot) {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED: // or 'paused'
+              console.log('Upload is paused');
+              break;
+            case firebase.storage.TaskState.RUNNING: // or 'running'
+              console.log('Upload is running');
+              break;
+          }
+        }, function(error) {
+          switch (error.code) {
+            case 'storage/unauthorized':
+              // User doesn't have permission to access the object
+              break;
+
+            case 'storage/canceled':
+              // User canceled the upload
+              break;
+
+            case 'storage/unknown':
+              // Unknown error occurred, inspect error.serverResponse
+              break;
+          }
+        }, function() {
+          // Upload completed successfully, now we can get the download URL
+          var downloadURL = uploadTask.snapshot.downloadURL;
+          return saveArticle(downloadURL,article);
+        });
     },
 
     // remove: function(Article) {
