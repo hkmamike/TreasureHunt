a1719f27382d0a8cd7372cc6fc2e639f230b21bd
AK <AKwoKWH+Github@gmail.com>
2017-04-16 17:57:00 +0800

    add auto count in firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,34 @@
-var functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+// Keeps track of the length of the 'likes' child list in a separate property.
+exports.countlikechange = functions.database.ref('/users/{userID}/{missionID}/tokenClaimed/{tokenID}').onWrite(event => {
+  const collectionRef = event.data.ref.parent;
+  const countRef = collectionRef.parent.child('tokenCount');
+
+  // Return the promise from countRef.transaction() so our function 
+  // waits for this async event to complete before it exits.
+  return countRef.transaction(current => {
+    if (event.data.exists() && !event.data.previous.exists()) {
+      return (current || 0) + 1;
+    }
+    else if (!event.data.exists() && event.data.previous.exists()) {
+      return (current || 0) - 1;
+    }
+  });
+});
+
+// If the number of likes gets deleted, recount the number of likes
+exports.recountlikes = functions.database.ref('/users/{userID}/{missionID}/tokenCount/').onWrite(event => {
+  if (!event.data.exists()) {
+    const counterRef = event.data.ref;
+    const collectionRef = counterRef.parent.child('tokenClaimed');
+    
+    // Return the promise from counterRef.set() so our function 
+    // waits for this async event to complete before it exits.
+    return collectionRef.once('value')
+        .then(messagesData => counterRef.set(messagesData.numChildren()));
+  }
+});
